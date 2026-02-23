2135b2232e67d132610e47e4dc63bc9300b29547
allohamora <herman30698@gmail.com>
2026-02-23 18:55:09 +0200

    fix: reset password visibility on record removal

--- src/components/unlock/unlock.form.tsx
+++ src/components/unlock/unlock.form.tsx
@@ -78,6 +78,7 @@ export const UnlockForm = ({ recordsReloadToken, setSession, reloadRecords }: Un
 
   const handleRemove = () => {
     reloadRecords();
+    setShowPassword(false);
     reset();
   };
 
