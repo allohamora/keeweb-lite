fb734d0e7c103971109999669d2c128a2e5a3be9
allohamora <herman30698@gmail.com>
2026-02-23 20:48:31 +0200

    feat: add listKdbxFiles

--- src/repositories/google-drive.repository.ts
+++ src/repositories/google-drive.repository.ts
@@ -1,6 +1,8 @@
 import { Lock } from '@/utils/lock.utils';
 import { PUBLIC_GOOGLE_CLIENT_ID } from 'astro:env/client';
 
+const DRIVE_API_BASE = 'https://www.googleapis.com/drive/v3';
+
 const lock = new Lock('google-drive-repository');
 
 class Auth {
@@ -55,4 +57,29 @@ class Auth {
 
 const auth = new Auth();
 
-console.log(auth);
+export type KdbxFile = {
+  id: string;
+  name: string;
+  modifiedTime: string;
+};
+
+export const listKdbxFiles = async () => {
+  const accessToken = await auth.getAccessToken();
+
+  const params = new URLSearchParams({
+    q: "name contains '.kdbx' and trashed=false",
+    fields: 'files(id,name,modifiedTime)',
+    orderBy: 'name',
+  });
+
+  const response = await fetch(`${DRIVE_API_BASE}/files?${params}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to list kdbx files: ${response.status} ${response.statusText}`);
+  }
+
+  const { files } = (await response.json()) as { files: KdbxFile[] };
+  return { files };
+};
