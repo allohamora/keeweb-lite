31102c4488bfc931d7497057df11756cfdb97aa0
allohamora <herman30698@gmail.com>
2026-02-24 20:52:46 +0200

    feat: add Google Identity mock

--- __tests__/setup-unit-context.ts
+++ __tests__/setup-unit-context.ts
@@ -2,4 +2,5 @@ import 'navigator.locks';
 import 'fake-indexeddb/auto';
 import './mocks/argon2-browser.mock';
 import './mocks/astro-env.mock';
+import './mocks/google-identity.mock';
 import './mocks/msw.mock';
